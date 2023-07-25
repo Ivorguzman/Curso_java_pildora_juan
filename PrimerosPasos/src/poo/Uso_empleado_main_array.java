b5c70e8f57d762eb501130d5f7486f2f5ae17aae
ivorguzman <ivorguzman7@gmail.com>
2023-07-25 07:38:55 -0400

    25072023

--- PrimerosPasos/src/poo/Uso_empleado_main_array.java
+++ PrimerosPasos/src/poo/Uso_empleado_main_array.java
@@ -3,31 +3,65 @@ package poo;
 import java.util.Date;
 import java.util.GregorianCalendar;
 
+
+
+
 /*
  * Se esta relizando todo el ejercicio en un solo archivo en otras palabras se cola colaron todos
  * los metdo clase en un solo fichero (Todas la clases main y demas)
  *
  * */
 
+// Clase EmpleadoArry
 class EmpleadoArry {
+
 
 	// Varaibles de Instancia
 	private String nombre;// primitivo
 	private Double sueldo;// primitivo
 	private Date altaContrato;// referenciado (Tipo Objeto)
+	private int id;
 
+	// Varaibles de Clase (static)
+	private static int contador = 0;// por ... se inicia en cero
 
-	// ***************** CONSTRUCTOR **************************
+	/*
+	 * ** CONSTRUCTOR firma -_Numero ,tipo, y orden de parametro_- (String nombre,
+	 * double sueldo, int ahno,int mes, int,dia) **
+	 */
 	public EmpleadoArry(String nombre, double sueldo, int ahno, int mes, int dia) {
+		System.out.println("29 this ==>" + this);
+		System.out.println("------------------------------------------------------");
 
+		++contador;
+		this.id = contador;
 		this.nombre = nombre;
 		this.sueldo = sueldo;
-		GregorianCalendar calendario = new GregorianCalendar(ahno, mes - 1, dia);// sobre carga del constructor
+		GregorianCalendar calendario = new GregorianCalendar(ahno, mes - 1, dia); // sobre carga del constructor
 		this.altaContrato = calendario.getTime();
+		System.out.println("");
 	}
-	// ***************** FIN DEL CONSTRUCTOR **************************
+
+
+	// ** SOBRE CARGA DEL CONSTRUCTOR firma -Numero ,tipo, y  orden  de  parametro- EmpleadoArry(String nombre) ***
+	public EmpleadoArry(String nombre) {
+
+		this(nombre, 30000, 2000, 01, 01); // Invocando constructor de la misma (clase tercer uso del this).
+		
+	}
+
+	// ** SOBRE CARGA DEL CONSTRUCTOR firma -_Numero ,tipo, y orden de parametro_-
+	public EmpleadoArry() { // Constructor por defecto << Sin Parametros >>
+		++contador;
+		this.id = contador;
+
+	}
 
 	// GETTER Y SETTERS
+	public int getId() {
+		return this.id;
+	}
+	;
 	public String getNombre() {
 		return this.nombre;
 	}
@@ -38,7 +72,7 @@ class EmpleadoArry {
 		}
 	}
 
-	public Double getSueldo() {
+	public double getSueldo() {
 		return this.sueldo;
 	}
 
@@ -60,14 +94,71 @@ class EmpleadoArry {
 
 	public void setSubeSueldo(double porcentaje) {
 
-		if (porcentaje > 0) {
-			double aumento = this.sueldo * porcentaje / 100;
-			this.sueldo += aumento;
+		if (porcentaje < 0) {
+			throw new IllegalArgumentException("Monto tiene que ser  positivo < Mayor a 0 >");
 		}
+		double aumento = this.sueldo * porcentaje / 100;
+		this.sueldo += aumento;
 	}
 	// Fin de GETTERS Y SETTERS
 
+
 }
+// Fin Clase EmpleadoArry
+
+
+// Clase Jefatura
+class Jefatura extends EmpleadoArry {
+
+	// CONSTRUCTOR DE LA CLASE
+	public Jefatura(String nombre, double sueldo, int ahno, int mes, int dia) {
+
+		super(nombre, sueldo, ahno, mes, dia);
+
+	};
+
+	private double incentivo;
+	
+	// GETTERS Y SETTERS
+
+@Override
+public double getSueldo() { // realazandolo sobre escribiendo el metodo getSueldo()
+	
+	double sueldoJefe = super.getSueldo(); // Invocando el metodo getSueldo de la clase Padre
+	return sueldoJefe + this.incentivo;
+}
+
+
+/*
+ * public double SueldoJefe() {// realazandolo creando el metodo SueldoJefe()
+ * double sueldoJefe = super.getSueldo(); // Invocando el metodo getSueldo de la
+ * clase Padre return sueldoJefe + this.incentivo;
+ * 
+ * }
+ */
+
+	public void setIncentivo(double incentivo) {
+		if (incentivo < 0) {
+			throw new IllegalArgumentException("Monto tiene que ser  positivo < Mayor a 0 >");
+		}
+		this.incentivo = incentivo;
+	}
+	// Fin de GETTERS Y SETTERS
+
+
+
+	public double getIncentivo() {
+		return this.incentivo;
+	}
+
+
+}
+// Fin clase Jefetura
+
+
+
+
+
 
 // ********* CLASE PRINCIPAL (main) ****************
 
@@ -75,22 +166,22 @@ public class Uso_empleado_main_array {
 
 	public static void main(String[] args) {
 
-		EmpleadoArry[] misEmpleado = new EmpleadoArry[3];
+		EmpleadoArry[] misEmpleados = new EmpleadoArry[6];
+		Jefatura[] misJefes = new Jefatura[2];
 
-		misEmpleado[0] = new EmpleadoArry("Ivor Guzmán", 85000, 1990, 12, 17);
-		misEmpleado[1] = new EmpleadoArry("Ana López", 95000, 1995, 06, 02);
-		misEmpleado[2] = new EmpleadoArry("Maria Martín", 105000, 2002, 03, 15);
+		
+
 
 		/*
-		 * System.out.println(misEmpleado[0].nombre);
-		 * System.out.println(misEmpleado[0].altaContrato);
+		 * System.out.println(misEmpleado[0].nombre); // El indice tiene atributos
+		 * [0].XXXXX System.out.println(misEmpleado[0].altaContrato);
 		 * System.out.println(misEmpleado[0].sueldo);
 		 * System.out.println(misEmpleado[1].nombre);
 		 * System.out.println(misEmpleado[1].altaContrato);
 		 * System.out.println(misEmpleado[1].sueldo);
 		 * System.out.println(misEmpleado[2].nombre);
 		 * System.out.println(misEmpleado[2].altaContrato);
-		 * System.out.println(misEmpleado[02].sueldo);
+		 * System.out.println(misEmpleado[2].sueldo);
 		 */
 
 		/*
@@ -112,35 +203,102 @@ public class Uso_empleado_main_array {
 		 * System.out.println(misEmpleado[0].nombre);
 		 */
 
+		misEmpleados[0] = new EmpleadoArry("Ivor Guzmán", 85000, 1990, 12, 17); // almasenado objetos de super-clase <
+		misEmpleados[1] = new EmpleadoArry("Ana López", 95000, 1995, 06, 02);
+		misEmpleados[2] = new EmpleadoArry("Maria Martín", 105000, 2002, 03, 15);
+		misEmpleados[3] = new EmpleadoArry("Alexande Guzmán"); // Constructoe sobrecargado
+		// misEmpleado[4] = new EmpleadoArry(); // Constructor sobre cargado.
+
+		System.out.println("******************************************************");
+		System.out.println("");
+
+
+
+		// *********** POLIMORFISMO ( Principio de sustitucion) **************
+		Jefatura jefe_RRHH = new Jefatura("Alex Guzmán", 55000, 2006, 9, 25);
+		jefe_RRHH.setIncentivo(2570);
+
+		misEmpleados[4] = jefe_RRHH;// almasenado objetos de sub-clase < HIJA >(Jefatura)
+		misEmpleados[5] = new Jefatura("Maria", 95000, 1999, 5, 26);// almasenado objetos de sub clase HIJA (Jefatura)
+
+
+
 		/*
-		 * // **** BUCLE FOR TRADICIONAL *****
-		 * 
-		 * for (int i = 0; i < misEmpleado.length; i++) {
+		 * // **** BUCLE FOR TRADICIONAL ***** for (int i = 0; i < misEmpleado.length;
+		 * i++) {
 		 * 
 		 * misEmpleado[i].setSubeSueldo(5);
 		 * 
+		 * System.out.println("misEmpleado:" + misEmpleado[i].getId());
+		 * System.out.println("Id :" + misEmpleado[i].getId());
 		 * System.out.println("Nombre :" + misEmpleado[i].getNombre());
 		 * System.out.println("Sueldo :" + misEmpleado[i].getSueldo());
 		 * System.out.println("Fecha de alta :" + misEmpleado[i].getAltaContrato());
 		 * System.out.println("************************************************");
-		 * System.out.println("");
+		 * System.out.println(""); }
 		 * 
-		 * }
 		 */
 
-		for (EmpleadoArry e : misEmpleado) {
+		System.out.printf("      Insetivo para los Jefes ==>  %.2f%n%n ", jefe_RRHH.getIncentivo());
 
-			e.setSubeSueldo(5);
-			System.out.println(e);
+		for (EmpleadoArry e : misEmpleados) {
+			// Variable etipo empleado ==> EmpleadoArry (e)
+			e.setSubeSueldo(5);// Metodo para aumentar el sueldo 5%
 
+			System.out.println("Valor de  e ==> " + e);
+			System.out.println("Valor de  misEmpleado ==> " + misEmpleados);
+			System.out.println("Id:" + e.getId());
 			System.out.println("Nombre :" + e.getNombre());
-			System.out.println("Sueldo :" + e.getSueldo());
+
+			/*
+			 * Llama al metodo getSueldo() de EmpleadoArry Ó Llama al metodo getSueldo() de
+			 * Jefatura en: misEmpleados[5] y misEmpleados[6] segun contexto. (ENLAZADO
+			 * DINAMICO DE LA MVJ)
+			 */
+			System.out.printf("Sueldo %.2f$%n:", e.getSueldo());
+
+
 			System.out.println("Fecha de alta :" + e.getAltaContrato());
 			System.out.println("************************************************");
 			System.out.println("");
-
 		}
+
+
+
 
 	}
 
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
