f3f9b9d99dbc5d5af98ca5f843580078bd8c2867
ivorguzman <ivorguzman7@gmail.com>
2023-09-16 04:43:56 -0400

    11092023

---
+++ V202_227_BBDD_JDBC/src/bbdb_JDBC/practicaGuiada_MVC_V210_217/principal/Ejecuta_Modelo_vista_controlador.java
@@ -0,0 +1,18 @@
+package bbdb_JDBC.practicaGuiada_MVC_V210_217.principal;
+
+import javax.swing.WindowConstants;
+
+import bbdb_JDBC.practicaGuiada_MVC_V210_217.vista.Marco_Aplicacion2;
+
+public class Ejecuta_Modelo_vista_controlador
+{
+
+	public static void main(String[] args)
+	{
+		Marco_Aplicacion2 miMarco = new Marco_Aplicacion2();
+		miMarco.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		miMarco.setVisible(true);
+
+	}
+
+}
---
+++ V202_227_BBDD_JDBC/src/bbdb_JDBC/practicaGuiada_MVC_V210_217/vista/Marco_Aplicacion2.java
@@ -0,0 +1,51 @@
+package bbdb_JDBC.practicaGuiada_MVC_V210_217.vista;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+
+@SuppressWarnings("serial")
+public class Marco_Aplicacion2 extends JFrame
+{
+
+	// ***** CONSTRUCTOR *****
+	@SuppressWarnings("unchecked")
+	public Marco_Aplicacion2()
+	{
+
+		this.setTitle("Consulta BBDD");
+		this.setBounds(500, 300, 400, 400);
+		this.setLayout(new BorderLayout());
+		JPanel menus = new JPanel();
+		menus.setLayout(new FlowLayout());
+		this.secciones = new JComboBox();
+		this.secciones.setEditable(false);
+		this.secciones.addItem("Todos");
+		this.paises = new JComboBox();
+		this.paises.setEditable(false);
+		this.paises.addItem("Todos");
+		this.resultado = new JTextArea(4, 50);
+		this.resultado.setEditable(false);
+		this.add(this.resultado);
+		menus.add(this.secciones);
+		menus.add(this.paises);
+		this.add(menus, BorderLayout.NORTH);
+		this.add(this.resultado, BorderLayout.CENTER);
+		JButton botonConsulta = new JButton("Consulta");
+		this.add(botonConsulta, BorderLayout.SOUTH);
+
+
+	}
+
+
+	// CAMPOS DE CLASES DE LOS JCOMBOOX
+
+	private JComboBox secciones;
+	private JComboBox paises;
+	private JTextArea resultado;
+}
